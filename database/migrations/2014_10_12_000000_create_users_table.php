cd25da34d3129d331c2e939f8e6be789dd561d75
fajarmagsyar <fajar.magsyar@gmail.com>
2022-03-28 15:38:23 +0800

    minor bug on migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateUsersTable extends Migration
 {
@@ -13,15 +14,21 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        DB::statement('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->uuid('user_id')->primary();
+            $table->uuid('role_id')->nullable();
+            $table->uuid('no_identitas')->nullable();
+            $table->string('nama')->nullable();
+            $table->string('jk')->nullable();
+            $table->string('no_hp', 15)->nullable();
+            $table->string('alamat')->nullable();
+            $table->string('npwp', 16)->nullable();
+            $table->string('email')->nullable();
+            $table->string('password')->nullable();
             $table->timestamps();
         });
+        DB::statement('ALTER TABLE users ALTER COLUMN user_id SET DEFAULT uuid_generate_v4();');
     }
 
     /**
